3169170247330abfe8f8f38f15d4332b4df35788
Yinghui Wang <May@Yinghuis-MacBook-Pro.local>
2015-11-02 14:24:59 -0800

    Initial Commit

---
+++ helloBillbo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  helloBillbo
+//
+//  Created by Yinghui Wang on 15/11/2.
+//  Copyright (c) 2015年 Yinghui Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
