55e9f4324ea846586c9cecf7c272e41d7ed8e35e
mayar1 <HH@MaytekiiMac.local>
2015-12-13 14:28:10 -0800

    Xcode for billbo

--- helloBillbo/ViewController.swift
+++ helloBillbo/ViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webView: UIWebView!
+    
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = NSURL(string:"http:/www.mayartstudio.com/617billbofinal/index.html");
+        let requestObj = NSURLRequest(URL: url!);
+        webView.loadRequest(requestObj);
+        
     }
 
     override func didReceiveMemoryWarning() {
